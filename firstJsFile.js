3d00bd4d4d5148c43f1a12a526ada02b454b9e4c
zhmurko-marina <zhmurko.marina@gmail.com>
2016-11-01 04:11:52 +0200

    Added: checking if inputed values in array are valid

--- firstJsFile.js
+++ firstJsFile.js
@@ -2,8 +2,14 @@
  * Created by Marina on 22.10.2016.
  */
 
-var array = [5, 4, 1, -8, 33, 55, 1, 3, 2, 88, 4, -3, -16, 5, -17, 1];
-
+var array = [1, NaN, 3, 5, -3];
+/*
+    [3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2 - 3 - 1]
+    [-1, -8, -2]
+    [1, 7, 3]
+    [1, undefined, 3, 5, -3]
+    [1, NaN, 3, 5, -3]
+*/
 var sum = 0,
     min = array[0],
     max = array[0],
@@ -11,7 +17,12 @@ var sum = 0,
     i;
 
 for (i = 0; i < n; i++) {
-    sum += array[i];
+    if (array[i] == undefined || isNaN(array[i]) == true) {
+        sum += 0;
+    }
+    else {
+        sum += array[i];
+    }
 }
 
 for (i = 1; i < n; i++) {
